289473e361d90fed46c8c87efd6720943276e367
Ol1v3rL <120492092+Ol1v3rL@users.noreply.github.com>
2022-12-13 17:13:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ streamlit.title('My first Streamlit App with GitHub')
 
 streamlit.header('This is a Header')
 streamlit.text('Menu 1')
-streamlit.txt('Menu 2')
+streamlit.text('Menu 2')
 streamlit.text('Menu 3')
