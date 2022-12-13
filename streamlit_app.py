6c11d926834168a41e3598bd6d6a6464c171c61a
Ol1v3rL <120492092+Ol1v3rL@users.noreply.github.com>
2022-12-13 17:14:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 streamlit.title('My first Streamlit App with GitHub')
 
-streamlit.header('This is a Header')
-streamlit.text('Menu 1')
-streamlit.text('Menu 2')
-streamlit.text('Menu 3')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
