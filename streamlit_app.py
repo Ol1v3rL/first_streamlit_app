b0a0c48e240e0454c89913e6fce4b45b28befbd1
Ol1v3rL <120492092+Ol1v3rL@users.noreply.github.com>
2022-12-17 14:26:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+
+
 
 streamlit.title('My first Streamlit App with GitHub')
 
@@ -20,6 +23,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+ 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
    
    
 # Display the table on the page.
